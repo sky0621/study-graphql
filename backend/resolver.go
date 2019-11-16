363359027a82a3178e8c8d647c1e55c0050b9542
sky0621 <sky14work@gmail.com>
2019-11-16 12:57:22 +0900

    backend

--- backend/resolver.go
+++ backend/resolver.go
@@ -16,11 +16,38 @@ func (r *Resolver) Query() QueryResolver {
 type mutationResolver struct{ *Resolver }
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input NewTodo) (*Todo, error) {
-	panic("not implemented")
+	return &Todo{
+		ID:   "todo001",
+		Text: "部屋の掃除",
+		Done: false,
+		User: &User{
+			ID:   "user001",
+			Name: "たろー",
+		},
+	},nil
 }
 
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*Todo, error) {
-	panic("not implemented")
+	return []*Todo{
+		&Todo{
+			ID:   "todo001",
+			Text: "部屋の掃除",
+			Done: false,
+			User: &User{
+				ID:   "user001",
+				Name: "たろー",
+			},
+		},
+		&Todo{
+			ID:   "todo002",
+			Text: "買い物",
+			Done: true,
+			User: &User{
+				ID:   "user001",
+				Name: "たろー",
+			},
+		},
+	},nil
 }
